a4d8f42dec705935905e8fee827629bb0f0c4b1b
Daniel Mateu <danielmateu86@gmail.com>
2023-10-09 16:46:20 +0200

    secuencias de escape

---
+++ scuencias_de_escape.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    /*
+        nulo -> \0
+        tabulador -> \t
+        retroceso -> \b
+        nueva linea -> \n
+        salto de pagina -> \f
+        retorno de carro -> \r
+        comilla simple -> \'
+        comilla doble -> \"
+        signo de interrogacion -> \?
+        barra invertida -> \\
+    */
+    printf("Bienvenido/as\n");
+    printf("\tBienvenido/as al curso \"C para gobernar el mundo\"\n");
+    return 0;
+}
