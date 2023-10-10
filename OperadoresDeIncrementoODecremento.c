5b349c5d80cfa65de83e6c9fb0fef52f401d6ffd
Daniel Mateu <danielmateu86@gmail.com>
2023-10-10 10:24:44 +0200

    operadores

---
+++ OperadoresDeIncrementoODecremento.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+int main(){
+    int num1 = 5;
+    num1++; // num1 = num1 + 1;
+    printf("num1 = %d\n", num1);
+    return 0;
+}
---
+++ OperadoresSimplificados.c
@@ -0,0 +1,20 @@
+int num1 = 10;
+int num2 = 20;
+
+// Operador de atribucion
+num1 = num2;
+
+// Operador de suma y asignacion
+num1 += num2;
+
+// Operador de resta y asignacion
+num1 -= num2; // num1 = num1 - num2;
+
+// Operador de multiplicacion y asignacion
+num1 *= num2; // num1 = num1 * num2;
+
+// Operador de division y asignacion
+num1 /= num2; // num1 = num1 / num2;
+
+// Operador de modulo y asignacion
+num1 %= num2; // num1 = num1 % num2;
