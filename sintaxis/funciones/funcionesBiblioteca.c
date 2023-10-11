46c6af3c7c528ed681e6153f548d38cd51e14c50
Daniel Mateu <danielmateu86@gmail.com>
2023-10-11 12:21:24 +0200

    funciones de biblioteca

---
+++ sintaxis/funciones/funcionesBiblioteca.c
@@ -0,0 +1,36 @@
+
+/*
+    Funciones de biblioteca -> Funciones que ya están definidas en C
+
+    <stdio.h>   // Entrada y salida de datos
+        -printf()
+        -scanf()
+        -getchar()
+        -putchar()
+
+    <math.h>
+        -sqrt()
+        -pow()
+        -abs()
+        -floor()
+        -ceil()
+        -exp()
+        -log()
+        -log10()
+        -sin()
+        -cos()
+        -tan()
+        -asin()
+        -acos()
+        -atan()
+        -atan2()
+
+    <ctype.h>
+        -isalpha()
+        -isdigit()
+        -islower()
+        -isupper()
+        -tolower()
+        -toupper()
+
+*/
